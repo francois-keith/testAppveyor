f382fa692198849b4a696cb9cc6d51d783831008
Escande Adrien <adrien.escande@gmail.com>
2016-05-26 12:26:38 +0900

    update

--- installAsAppveyorDependency.ps1
+++ installAsAppveyorDependency.ps1
@@ -3,11 +3,7 @@ $aprutil_url = "http://ftp.meisei-u.ac.jp/mirror/apache/dist//apr/apr-util-1.5.4
 $log4cxx_url = "http://ftp.meisei-u.ac.jp/mirror/apache/dist/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.zip"
 
 pushd $Env:SOURCE_FOLDER
-if((Test-Path $Env:SOURCE_FOLDER/download) -eq 0)
-{
-  md download
-}
-cd download
+
 (New-Object System.Net.WebClient).DownloadFile($apr_url, "apr.zip")
 (New-Object System.Net.WebClient).DownloadFile($aprutil_url, "aprutil.zip")
 (New-Object System.Net.WebClient).DownloadFile($log4cxx_url, "log4cxx.zip")
@@ -17,9 +13,9 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory("aprutil.zip","apr-util")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("log4cxx.zip","log4cxx")
 
-Move-Item -path $Env:SOURCE_FOLDER/download/apr/apr-1.5.2 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr
-Move-Item -path $Env:SOURCE_FOLDER/download/apr-util/apr-util-1.5.4 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr-util
-Move-Item -path $Env:SOURCE_FOLDER/download/log4cxx/apache-log4cxx-0.10.0 -destination $Env:SOURCE_FOLDER/log4cxxWin32/log4cxx
+Move-Item -path \[Environment\]::CurrentDirectory/apr/apr-1.5.2 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr
+Move-Item -path \[Environment\]::CurrentDirectory/apr-util/apr-util-1.5.4 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr-util
+Move-Item -path \[Environment\]::CurrentDirectory/log4cxx/apache-log4cxx-0.10.0 -destination $Env:SOURCE_FOLDER/log4cxxWin32/log4cxx
 
 # cloning git repo into an already existing folder
 cd $Env:SOURCE_FOLDER/log4cxxWin32
