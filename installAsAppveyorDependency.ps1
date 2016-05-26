1c3bd67744a2c3753771d96429d69bc9ea5285ff
Escande Adrien <adrien.escande@gmail.com>
2016-05-26 15:23:24 +0900

    update

--- installAsAppveyorDependency.ps1
+++ installAsAppveyorDependency.ps1
@@ -1,6 +1,8 @@
+$workingDir = [environment]::CurrentDirectory
 $apr_url = "http://ftp.meisei-u.ac.jp/mirror/apache/dist//apr/apr-1.5.2-win32-src.zip" 
 $aprutil_url = "http://ftp.meisei-u.ac.jp/mirror/apache/dist//apr/apr-util-1.5.4-win32-src.zip"
 $log4cxx_url = "http://ftp.meisei-u.ac.jp/mirror/apache/dist/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.zip"
+
 
 pushd $Env:SOURCE_FOLDER
 
@@ -13,9 +15,9 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory("aprutil.zip","apr-util")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("log4cxx.zip","log4cxx")
 
-Move-Item -path [environment]::CurrentDirectory/apr/apr-1.5.2 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr
-Move-Item -path [environment]::CurrentDirectory/apr-util/apr-util-1.5.4 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr-util
-Move-Item -path [environment]::CurrentDirectory/log4cxx/apache-log4cxx-0.10.0 -destination $Env:SOURCE_FOLDER/log4cxxWin32/log4cxx
+Move-Item -path $workingDir/apr/apr-1.5.2 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr
+Move-Item -path $workingDir/apr-util/apr-util-1.5.4 -destination $Env:SOURCE_FOLDER/log4cxxWin32/apr-util
+Move-Item -path $workingDir/log4cxx/apache-log4cxx-0.10.0 -destination $Env:SOURCE_FOLDER/log4cxxWin32/log4cxx
 
 # cloning git repo into an already existing folder
 cd $Env:SOURCE_FOLDER/log4cxxWin32
